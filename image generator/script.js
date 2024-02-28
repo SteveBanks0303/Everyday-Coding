08a73a50d984243b53d6e06743656e4a1371cfca
SteveBanks0303 <stephenbankole03@gmail.com>
2024-02-28 03:55:59 +0100

    Update

---
+++ image generator/script.js
@@ -0,0 +1,8 @@
+const generateForm = document.querySelector(".generate-form");
+
+const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log(e.srcElement);
+}
+
+generateForm.addEventListener("submit", handleFormSubmission);
